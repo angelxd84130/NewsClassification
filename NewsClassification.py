33b24358ee29382fdf7f903f6006844432bc7a6e
angel <angelxd84130@gmail.com>
2021-03-15 19:42:31 -0700

    load data and get result

--- NewsClassification.py
+++ NewsClassification.py
@@ -1,42 +1,8 @@
-# using deep learning_RNN
-from newsapi import NewsApiClient
-import numpy as np
-import pandas as pd
-newsapi = NewsApiClient(api_key='9cd9ca0dc6ec44388be32fb87220cb75')
-#Alltop = ["science", "general", "health", "business", "entertainment", "sports"]
-Alltop = ["health", "business", "entertainment", "sports"]
-TopNum = {}
-data = np.array([])
-Texts = []
-Labels = []
-page_size = 100
+# using method
 
-
-def getPlan(data, idx, Texts, Labels, TopNum):
-    TopNum[Alltop[idx]] = 0
-    for j in range(len(data)):
-        if data[j]['content'] is not None:
-            # clean data (get content only)
-            #print([data[j]['content']])
-            Texts.append(data[j]['content'])
-            # set up labels (categories)
-            Labels.append(idx)
-            TopNum[Alltop[idx]] += 1
-    #print("Here", Texts)
-    #return np.array(Texts)
-
-
-for i in range(len(Alltop)):
-    data = np.array([])
-    # get all data from newsAPI
-    data = newsapi.get_top_headlines(category=Alltop[i], language='en', country="us", page_size=page_size).get('articles')
-    getPlan(data, i, Texts, Labels, TopNum)
-    #print(data)
-
-# check how many data we have
-print("Articles:", TopNum)
-#print("Lebel", Labels)
-#print("Text:\n", Texts)
+# get data
+from news import news_data
+(trainX, trainY), (testX, testY) = news_data()
 
 # address Texts
 # design how to remove punctuation from every sentence
