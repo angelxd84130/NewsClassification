8d6faa2d778eb998a62d21ba5aad5fa27801f62c
angel <angelxd84130@gmail.com>
2021-03-29 14:12:19 -0700

    run in json file

--- news.py
+++ news.py
@@ -11,18 +11,18 @@ def countData(df):
     for i in Alltop:
         TopNum[i] = 0
     for j in range(len(df)):
-        TopNum[Alltop[df[j]['label']]] += 1
+        TopNum[Alltop[df.iloc[j]['label']]] += 1
     print("Articles:", TopNum)
 
 def news_data():
     print("Get news articles from newsapi.NewsApiClient in 6 categories")
-    for i in range(len(Alltop)):
-        # get all data from newsAPI
-        df = pd.read_json("data.json")
-        countData(df)
+
+    # get all data from json file
+    df = pd.read_json("data.json")
+    countData(df)
 
     # make training data and testing data
     from sklearn.model_selection import train_test_split
     trainX, testX, trainY, testY = train_test_split(df[:]['content'], df[:]['label'], test_size=0.25, random_state=1000)
     return (trainX, trainY), (testX, testY)
-#news_data()
+news_data()
